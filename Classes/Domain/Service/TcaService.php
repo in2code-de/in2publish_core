0678ce3f8f53a5eb349edc991772feabdf20cb82
Oliver Eglseder <oliver.eglseder@in2code.de>
2016-09-20 11:54:13 +0200

    [BUGFIX] Ignore TCA columns without config section

--- Classes/Domain/Service/TcaService.php
+++ Classes/Domain/Service/TcaService.php
@@ -182,6 +182,11 @@ class TcaService
             }
 
             foreach ($tableConfiguration[self::COLUMNS] as $column => $columnConfiguration) {
+                // if the column has no config section like sys_file_metadata[columns][height]
+                if (!isset($columnConfiguration[self::CONFIG])) {
+                    continue;
+                }
+
                 $config = $columnConfiguration[self::CONFIG];
                 $type = $config[self::TYPE];
 
