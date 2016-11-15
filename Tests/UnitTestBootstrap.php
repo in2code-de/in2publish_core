7a050392e9d867ee773d134117b26d98e39e68d4
Oliver Eglseder <oliver.eglseder@in2code.de>
2016-11-15 10:02:56 +0100

    [TETS] Restructure UnitTestBootstrap

--- Tests/UnitTestBootstrap.php
+++ Tests/UnitTestBootstrap.php
@@ -1,18 +1,20 @@
 <?php
-
-if (!($webRoot = getenv('TYPO3_PATH_WEB'))) {
-    $webRoot = realpath(__DIR__ . '/../../../../') . '/';
-
-    putenv('TYPO3_PATH_WEB=' . $webRoot);
-
-    if (!file_exists($webRoot . 'vendor/autoload.php')) {
-        throw new \LogicException(
-            'Use this bootstrap file only when you installed in2publish via composer as dependency or predefine TYPO3_PATH_WEB'
-        );
-    }
+if (empty($webRoot = getenv('TYPO3_PATH_WEB'))) {
+    putenv('TYPO3_PATH_WEB=' . $webRoot = realpath(__DIR__ . '/../../../../') . '/');
+} else {
+    $webRoot = rtrim($webRoot, '/') . '/';
 }
 
+if (!file_exists($autoload = $webRoot . 'vendor/autoload.php') && !file_exists($autoload = $webRoot . '../vendor/autoload.php')) {
+    throw new \LogicException(
+        'Use this bootstrap file only when you installed in2publish via composer as dependency or predefine TYPO3_PATH_WEB'
+    );
+}
+
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['in2publish_core'] = array();
+
+require($autoload);
 
 $coreBootstrap = $webRoot . '/typo3/sysext/core/Build/UnitTestsBootstrap.php';
 
