d8357f4a07b38509c4fc5ac5a78e20ca5334d0f1
Oliver Eglseder <oliver.eglseder@in2code.de>
2017-05-04 13:05:27 +0200

    [FEATURE] Use caching for createMasks in SshConnection

--- Classes/Security/SshConnection.php
+++ Classes/Security/SshConnection.php
@@ -35,6 +35,7 @@ use In2code\In2publishCore\Communication\RemoteCommandExecution\RemoteCommandDis
 use In2code\In2publishCore\Service\Context\ContextService;
 use In2code\In2publishCore\Utility\ConfigurationUtility;
 use Psr\Log\LoggerInterface;
+use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
@@ -45,6 +46,7 @@ class SshConnection
 {
     const SSH2_WRAPPER = 'ssh2.sftp://';
     const TYPO3_CLI_EXTBASE_DISPATCHER = './typo3/cli_dispatch.phpsh extbase ';
+    const CREATE_MASKS_CACHE_KEY = 'create_masks';
 
     /**
      * @var array
@@ -789,66 +791,82 @@ class SshConnection
      */
     private function setCreateMasks()
     {
-        $command =
-            'cd ' . $this->foreignRootPath . ' && '
-            . $this->pathToPhp . ' '
-            . self::TYPO3_CLI_EXTBASE_DISPATCHER . StatusCommandController::CREATE_MASKS_COMMAND;
-        $response = $this->executeRemoteCommand($command);
-        if (!is_array($response)) {
-            throw new \Exception(
-                'Failed to retrieve foreign file and folder mask. The response is not an array.',
-                1440750956
-            );
-        }
+        if ($this->getCache()->has(self::CREATE_MASKS_CACHE_KEY)) {
+            $createMasks = $this->getCache()->get(self::CREATE_MASKS_CACHE_KEY);
+        } else {
+            $command =
+                'cd ' . $this->foreignRootPath . ' && '
+                . $this->pathToPhp . ' '
+                . self::TYPO3_CLI_EXTBASE_DISPATCHER . StatusCommandController::CREATE_MASKS_COMMAND;
+            $response = $this->executeRemoteCommand($command);
+            if (!is_array($response)) {
+                throw new \Exception(
+                    'Failed to retrieve foreign file and folder mask. The response is not an array.',
+                    1440750956
+                );
+            }
 
-        $createMasks = array();
-        foreach ($response as $value) {
-            $information = GeneralUtility::trimExplode(':', $value);
-            if ($information[0] === 'FileCreateMask') {
-                $createMasks['file'] = $information[1];
-            } elseif ($information[0] === 'FolderCreateMask') {
-                $createMasks['folder'] = $information[1];
-            } else {
-                if (false !== strpos($value, 'not accepted when the connection to the database')) {
-                    throw new \Exception(
-                        'The foreign database is unreachable. (CMD: "' . $command . '"; Message: "' . $value . '")',
-                        1476450079
-                    );
+            $createMasks = array();
+            foreach ($response as $value) {
+                $information = GeneralUtility::trimExplode(':', $value);
+                if ($information[0] === 'FileCreateMask') {
+                    $createMasks['file'] = $information[1];
+                } elseif ($information[0] === 'FolderCreateMask') {
+                    $createMasks['folder'] = $information[1];
                 } else {
-                    throw new \Exception(
-                        'Failed to retrieve foreign file and folder mask. '
-                        . 'The response does not contain the neccessary keys. '
-                        . 'Check if BE user _cli_lowlevel does exist on foreign, '
-                        . 'in2publish is installed and command controllers can be called (CMD: "'
-                        . $command . '"; Message: "' . $value . '")',
-                        1446816322
-                    );
+                    if (false !== strpos($value, 'not accepted when the connection to the database')) {
+                        throw new \Exception(
+                            'The foreign database is unreachable. (CMD: "' . $command . '"; Message: "' . $value . '")',
+                            1476450079
+                        );
+                    } else {
+                        throw new \Exception(
+                            'Failed to retrieve foreign file and folder mask. '
+                            . 'The response does not contain the neccessary keys. '
+                            . 'Check if BE user _cli_lowlevel does exist on foreign, '
+                            . 'in2publish is installed and command controllers can be called (CMD: "'
+                            . $command . '"; Message: "' . $value . '")',
+                            1446816322
+                        );
+                    }
                 }
             }
+
+            if (!isset($createMasks['folder'])) {
+                throw new \Exception('Failed to retrieve foreign folder folder mask.', 1440750975);
+            }
+            if (!isset($createMasks['file'])) {
+                throw new \Exception('Failed to retrieve foreign file folder mask.', 1440750975);
+            }
+            if (!is_string($createMasks['folder']) || preg_match('~^[0124567]{4}$~', $createMasks['folder']) !== 1) {
+                throw new \Exception(
+                    'The retrieved folder mask "' . htmlspecialchars($createMasks['folder']) . '" is invalid.',
+                    1440751666
+                );
+            }
+            if (!is_string($createMasks['file']) || preg_match('~^[0124567]{4}$~', $createMasks['file']) !== 1) {
+                throw new \Exception(
+                    'The retrieved file  mask "' . htmlspecialchars($createMasks['file']) . '" is invalid.',
+                    1440751662
+                );
+            }
+            // Cache this value for a day.
+            $this->getCache()->set(self::CREATE_MASKS_CACHE_KEY, $createMasks, [], 86400);
         }
 
-        if (!isset($createMasks['folder'])) {
-            throw new \Exception('Failed to retrieve foreign folder folder mask.', 1440750975);
-        }
-        if (!isset($createMasks['file'])) {
-            throw new \Exception('Failed to retrieve foreign file folder mask.', 1440750975);
-        }
-        if (!is_string($createMasks['folder']) || preg_match('~^[0124567]{4}$~', $createMasks['folder']) !== 1) {
-            throw new \Exception(
-                'The retrieved folder mask "' . htmlspecialchars($createMasks['folder']) . '" is invalid.',
-                1440751666
-            );
-        }
-        if (!is_string($createMasks['file']) || preg_match('~^[0124567]{4}$~', $createMasks['file']) !== 1) {
-            throw new \Exception(
-                'The retrieved file  mask "' . htmlspecialchars($createMasks['file']) . '" is invalid.',
-                1440751662
-            );
-        }
         $this->rawFileMode = $createMasks['file'];
         $this->rawFolderMode = $createMasks['folder'];
         $this->fileMode = octdec($createMasks['file']);
         $this->folderMode = octdec($createMasks['folder']);
+    }
+
+    /**
+     * @return \TYPO3\CMS\Core\Cache\Frontend\FrontendInterface
+     * @codeCoverageIgnore
+     */
+    private function getCache()
+    {
+        return GeneralUtility::makeInstance(CacheManager::class)->getCache('in2publish_core');
     }
 
     /**
@@ -885,6 +903,7 @@ class SshConnection
         if (function_exists('ssh2_sftp_chmod')) {
             $this->chmodEnabled = true;
         }
+
         $configuration = ConfigurationUtility::getConfiguration('sshConnection');
         if (!empty($configuration)) {
             $this->validateConfiguration($configuration);
