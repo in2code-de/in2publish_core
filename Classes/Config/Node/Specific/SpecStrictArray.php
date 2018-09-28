8a4b72d8ed5ae1b2361bef5e32c00de5e62b6247
Mark Watney <mark.watney@protonmail.com>
2018-09-28 17:03:35 +0200

    [TASK] Add missing strict type declarations

--- Classes/Config/Node/Specific/SpecStrictArray.php
+++ Classes/Config/Node/Specific/SpecStrictArray.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace In2code\In2publishCore\Config\Node\Specific;
 
 /***************************************************************
--- Classes/Features/SimpleOverviewAndAjax/Config/Definer/SimpleOverviewAndAjaxDefiner.php
+++ Classes/Features/SimpleOverviewAndAjax/Config/Definer/SimpleOverviewAndAjaxDefiner.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace In2code\In2publishCore\Features\SimpleOverviewAndAjax\Config\Definer;
 
 /***************************************************************
