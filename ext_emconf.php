63082ca013010b131fcedbbf315d863ef4985883
Oliver Eglseder <oliver.eglseder@in2code.de>
2019-06-27 16:13:49 +0200

    [UPDATE] Raise PHP version requirements to at least 7.2 (like TYPO3 v9 minimum)

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.0.0',
-            'php' => '7.0.0-7.3.99',
+            'php' => '7.2.0-7.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
