7b00073ea587e4940bfe4462a4cc3fa5070bfb9f
Oliver Eglseder <oliver.eglseder@in2code.de>
2020-04-23 20:36:12 +0200

    [CODESTYLE] Fix line breaks for long line in ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -54,7 +54,9 @@
     $configContainer->registerDefiner(
         \In2code\In2publishCore\Features\SimpleOverviewAndAjax\Config\Definer\SimpleOverviewAndAjaxDefiner::class
     );
-    $configContainer->registerDefiner(\In2code\In2publishCore\Features\WarningOnForeign\Config\Definer\WarningOnForeignDefiner::class);
+    $configContainer->registerDefiner(
+        \In2code\In2publishCore\Features\WarningOnForeign\Config\Definer\WarningOnForeignDefiner::class
+    );
     if ($contextService->isForeign()
         || 'ssh' === $extConf['adapter']['remote']
         || 'ssh' === $extConf['adapter']['transmission']
