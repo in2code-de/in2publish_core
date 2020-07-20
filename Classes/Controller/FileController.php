bac52d7adecde5f0000902ee812d37ffc3569da5
Oliver Eglseder <oliver.eglseder@in2code.de>
2020-07-20 10:13:30 +0200

    [BUGFIX] Run tasks after publishing files and folders
    Resolves: https://projekte.in2code.de/issues/40951

--- Classes/Controller/FileController.php
+++ Classes/Controller/FileController.php
@@ -81,6 +81,7 @@ class FileController extends AbstractController
     public function publishFolderAction($identifier)
     {
         $success = GeneralUtility::makeInstance(FolderPublisherService::class)->publish($identifier);
+        $this->runTasks();
 
         if ($success) {
             $this->addFlashMessage(
@@ -144,6 +145,7 @@ class FileController extends AbstractController
                     LocalizationUtility::translate('file_publishing.failure', 'in2publish_core')
                 );
             }
+            $this->runTasks();
         }
 
         try {
