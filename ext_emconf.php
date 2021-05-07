3c14cfc11d7979da71d3029aaa729e6c4c8a3944
Oliver Eglseder <oliver.eglseder@in2code.de>
2021-05-07 09:22:18 +0200

    [FEATURE] Set TYPO3 v11 as new dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'in2code.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.99.99',
-            'php' => '7.2.0-7.3.99',
+            'typo3' => '11.0.0-11.99.99',
+            'php' => '7.4.0-7.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
