f299534df2280fe646ba7dbb8a2357ac9882dd7e
Oliver Eglseder <oliver.eglseder@in2code.de>
2021-07-05 19:20:15 +0200

    [FEATURE] Replace shouldSkipFindByIdentifier signal with event

---
+++ Classes/Event/VoteIfFindingByIdentifierShouldBeSkipped.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace In2code\In2publishCore\Event;
+
+use In2code\In2publishCore\Domain\Repository\CommonRepository;
+
+class VoteIfFindingByIdentifierShouldBeSkipped extends AbstractVotingEvent
+{
+    /** @var CommonRepository */
+    protected $commonRepository;
+
+    /** @var int */
+    protected $identifier;
+
+    /** @var string */
+    protected $tableName;
+
+    public function __construct(CommonRepository $commonRepository, int $identifier, string $tableName)
+    {
+        $this->commonRepository = $commonRepository;
+        $this->identifier = $identifier;
+        $this->tableName = $tableName;
+    }
+
+    public function getCommonRepository(): CommonRepository
+    {
+        return $this->commonRepository;
+    }
+
+    public function getIdentifier(): int
+    {
+        return $this->identifier;
+    }
+
+    public function getTableName(): string
+    {
+        return $this->tableName;
+    }
+}
