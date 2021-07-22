8e838ae575ce260f0dab415251aa4838fbad0bd6
Oliver Eglseder <oliver.eglseder@in2code.de>
2021-07-22 11:59:02 +0200

    [BUGFIX] Allow null as return value in FalIndexPostProcessor::getStorage

--- Classes/Domain/PostProcessing/FalIndexPostProcessor.php
+++ Classes/Domain/PostProcessing/FalIndexPostProcessor.php
@@ -146,7 +146,7 @@ class FalIndexPostProcessor implements SingletonInterface
      *
      * @return ResourceStorage
      */
-    protected function getStorage(RecordInterface $record): ResourceStorage
+    protected function getStorage(RecordInterface $record): ?ResourceStorage
     {
         static $storages = [];
         if (!isset($storages[0])) {
