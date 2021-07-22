e8b3bd26c555c93a61cc3260511d0ad104595052
Oliver Eglseder <oliver.eglseder@in2code.de>
2021-07-22 12:57:16 +0200

    [CLEANUP] Remove unused imports and superfluous lines in import sections

--- Classes/Features/ContextMenuPublishEntry/Controller/PublishPageAjaxController.php
+++ Classes/Features/ContextMenuPublishEntry/Controller/PublishPageAjaxController.php
@@ -18,7 +18,6 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 use function json_encode;
-use function sprintf;
 
 class PublishPageAjaxController
 {
