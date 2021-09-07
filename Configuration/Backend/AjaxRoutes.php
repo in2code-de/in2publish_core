20f24ba7f1306bea49fdf5e9fa6ac5daebc65510
Oliver Eglseder <oliver.eglseder@in2code.de>
2021-09-07 14:39:55 +0200

    [REFACTOR] Import all classes in files where FQCNs are not required

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -1,8 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+use In2code\In2publishCore\Features\ContextMenuPublishEntry\Controller\PublishPageAjaxController;
+
 return [
     'in2publishcore_contextmenupublishentry_publish' => [
         'path' => '/tx_in2publishcore/contextmenupublishentry/publish',
-        'target' => \In2code\In2publishCore\Features\ContextMenuPublishEntry\Controller\PublishPageAjaxController::class . '::publishPage'
+        'target' => PublishPageAjaxController::class . '::publishPage'
     ]
 ];
