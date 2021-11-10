f1009d82d1cee9e534351e8d514e267d9d10b87d
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2021-11-10 15:39:03 +0100

    [UPGRADE] Copy deprecated QueryHelper and QueryGenerator functions to Database utility
    * as suggested in: https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/11.0/Deprecation-92080-DeprecatedQueryGeneratorAndQueryView.html

--- Classes/Utility/DatabaseUtility.php
+++ Classes/Utility/DatabaseUtility.php
@@ -28,6 +28,7 @@ namespace In2code\In2publishCore\Utility;
  * This copyright notice MUST APPEAR in all copies of the script!
  */
 
+use Doctrine\DBAL\Driver\Exception;
 use In2code\In2publishCore\Config\ConfigContainer;
 use In2code\In2publishCore\In2publishCoreException;
 use In2code\In2publishCore\Service\Environment\ForeignEnvironmentService;
@@ -36,6 +37,8 @@ use LogicException;
 use Throwable;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryHelper;
+use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Log\Logger;
 use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -346,4 +349,78 @@ class DatabaseUtility
     {
         return $connection->insert($tableName, $row);
     }
+
+    /**
+     * Copied from deprecated QueryGenerator
+     * see: Deprecation-92080-DeprecatedQueryGeneratorAndQueryView.html
+     * 
+     * Recursively fetch all descendants of a given page
+     *
+     * @param int $id uid of the page
+     * @param int $depth
+     * @param int $begin
+     * @param string $permClause
+     * @return string comma separated list of descendant pages
+     * @throws Exception
+     */
+    public static function getTreeList($id, $depth, $begin = 0, $permClause = '')
+    {
+        $depth = (int)$depth;
+        $begin = (int)$begin;
+        $id = (int)$id;
+        if ($id < 0) {
+            $id = abs($id);
+        }
+        if ($begin === 0) {
+            $theList = $id;
+        } else {
+            $theList = '';
+        }
+        if ($id && $depth > 0) {
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
+            $queryBuilder->getRestrictions()->removeAll()->add(GeneralUtility::makeInstance(DeletedRestriction::class));
+            $queryBuilder->select('uid')
+                ->from('pages')
+                ->where(
+                    $queryBuilder->expr()->eq('pid', $queryBuilder->createNamedParameter($id, \PDO::PARAM_INT)),
+                    $queryBuilder->expr()->eq('sys_language_uid', 0)
+                )
+                ->orderBy('uid');
+            if ($permClause !== '') {
+                $queryBuilder->andWhere(self::stripLogicalOperatorPrefix($permClause));
+            }
+            $statement = $queryBuilder->execute();
+            while ($row = $statement->fetchAssociative()) {
+                if ($begin <= 0) {
+                    $theList .= ',' . $row['uid'];
+                }
+                if ($depth > 1) {
+                    $theSubList =self::getTreeList($row['uid'], $depth - 1, $begin - 1, $permClause);
+                    if (!empty($theList) && !empty($theSubList) && ($theSubList[0] !== ',')) {
+                        $theList .= ',';
+                    }
+                    $theList .= $theSubList;
+                }
+            }
+        }
+        return $theList;
+    }
+
+    /**
+     * Copied from deprecated QueryHelper
+     * see: Deprecation-92080-DeprecatedQueryGeneratorAndQueryView.html
+     *
+     * Removes the prefixes AND/OR from the input string.
+     *
+     * This function should be used when you can't guarantee that the string
+     * that you want to use as a WHERE fragment is not prefixed.
+     *
+     * @param string $constraint The where part fragment with a possible leading AND or OR operator
+     * @return string The modified where part without leading operator
+     */
+    public static function stripLogicalOperatorPrefix(string $constraint): string
+    {
+        return preg_replace('/^(?:(AND|OR)[[:space:]]*)+/i', '', trim($constraint)) ?: '';
+    }
 }
+
