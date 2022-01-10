1bfe6c87a560de49d4a9137f82605b37719a9b15
Oliver Eglseder <oliver.eglseder@in2code.de>
2022-01-10 18:29:08 +0100

    [UPDATE] Require at least TYPO3 v11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.4.0-7.4.99',
-            'typo3' => '11.4.0-11.99.99',
+            'typo3' => '11.5.0-11.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
