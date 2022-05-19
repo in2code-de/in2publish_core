66f87d0708f9a01fb2f3533ffa6ca9e84a7f2fd9
Oliver Eglseder <oliver.eglseder@in2code.de>
2022-05-19 11:20:11 +0200

    [CLEANUP] Remove wrong copyright notices

--- Classes/Component/TcaHandling/PreProcessing/PreProcessor/FlexProcessor.php
+++ Classes/Component/TcaHandling/PreProcessing/PreProcessor/FlexProcessor.php
@@ -4,67 +4,16 @@ declare(strict_types=1);
 
 namespace In2code\In2publishCore\Component\TcaHandling\PreProcessing\PreProcessor;
 
-/*
- * Copyright notice
- *
- * (c) 2021 in2code.de and the following authors:
- * Oliver Eglseder <oliver.eglseder@in2code.de>
- *
- * All rights reserved
- *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- */
-
 use In2code\In2publishCore\Component\TcaHandling\PreProcessing\Service\FlexFormFlatteningService;
 use In2code\In2publishCore\Component\TcaHandling\Resolver\FlexResolver;
 use In2code\In2publishCore\Component\TcaHandling\Resolver\Resolver;
 use TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools;
-use TYPO3\CMS\Core\Service\FlexFormService;
 
 use function array_key_exists;
 use function array_keys;
 use function json_encode;
 
 use const JSON_THROW_ON_ERROR;
-
-/*
- * Copyright notice
- *
- * (c) 2016 in2code.de and the following authors:
- * Oliver Eglseder <oliver.eglseder@in2code.de>
- *
- * All rights reserved
- *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- */
 
 class FlexProcessor extends AbstractProcessor
 {
