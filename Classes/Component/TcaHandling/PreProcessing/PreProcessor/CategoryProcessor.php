0f29a33cba013b9acc9c9465b3163d57577606c4
Oliver Eglseder <oliver.eglseder@in2code.de>
2022-05-19 10:30:57 +0200

    [FEATURE] Add CategoryProcessor for categorized tables

---
+++ Classes/Component/TcaHandling/PreProcessing/PreProcessor/CategoryProcessor.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace In2code\In2publishCore\Component\TcaHandling\PreProcessing\PreProcessor;
+
+use In2code\In2publishCore\Component\TcaHandling\PreProcessing\Service\DatabaseIdentifierQuotingService;
+use In2code\In2publishCore\Component\TcaHandling\Resolver\Resolver;
+use In2code\In2publishCore\Component\TcaHandling\Resolver\SelectMmResolver;
+use TYPO3\CMS\Core\Database\Connection;
+
+class CategoryProcessor extends AbstractProcessor
+{
+    protected Connection $localDatabase;
+    protected DatabaseIdentifierQuotingService $databaseIdentifierQuotingService;
+    protected string $type = 'category';
+
+    public function injectLocalDatabase(Connection $localDatabase): void
+    {
+        $this->localDatabase = $localDatabase;
+    }
+
+    public function injectDatabaseIdentifierQuotingService(
+        DatabaseIdentifierQuotingService $databaseIdentifierQuotingService
+    ): void {
+        $this->databaseIdentifierQuotingService = $databaseIdentifierQuotingService;
+    }
+
+    protected function buildResolver(string $table, string $column, array $processedTca): ?Resolver
+    {
+        $escapedTable = $this->localDatabase->quoteIdentifier($table);
+
+        $additionalWhere = 'AND {#sys_category}.{#sys_language_uid} IN (-1, 0)
+             AND {#sys_category}.{#fieldname} = "categories"
+             AND {#sys_category}.{#tablenames} = "' . $escapedTable . '"';
+        $additionalWhere = $this->databaseIdentifierQuotingService->dododo($additionalWhere);
+
+        /** @var SelectMmResolver $resolver */
+        $resolver = $this->container->get(SelectMmResolver::class);
+        $resolver->configure(
+            $additionalWhere,
+            $column,
+            'sys_category_record_mm',
+            'sys_category',
+            'uid'
+        );
+        return $resolver;
+    }
+}
