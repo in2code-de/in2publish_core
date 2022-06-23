93ed31404fb08fdd496ee30b4358e3293b925d43
Oliver Eglseder <oliver.eglseder@in2code.de>
2022-06-23 11:53:18 +0200

    [META] Add phpstorm meta file

---
+++ .phpstorm.meta.php
@@ -0,0 +1,21 @@
+<?php
+namespace PHPSTORM_META {
+    expectedReturnValues(
+        \In2code\In2publishCore\Domain\Model\Record::getState(),
+        \In2code\In2publishCore\Domain\Model\Record::S_ADDED,
+        \In2code\In2publishCore\Domain\Model\Record::S_CHANGED,
+        \In2code\In2publishCore\Domain\Model\Record::S_MOVED,
+        \In2code\In2publishCore\Domain\Model\Record::S_SOFT_DELETED,
+        \In2code\In2publishCore\Domain\Model\Record::S_DELETED,
+        \In2code\In2publishCore\Domain\Model\Record::S_UNCHANGED
+    );
+    expectedReturnValues(
+        \In2code\In2publishCore\Domain\Model\AbstractRecord::getState(),
+        \In2code\In2publishCore\Domain\Model\Record::S_ADDED,
+        \In2code\In2publishCore\Domain\Model\Record::S_CHANGED,
+        \In2code\In2publishCore\Domain\Model\Record::S_MOVED,
+        \In2code\In2publishCore\Domain\Model\Record::S_SOFT_DELETED,
+        \In2code\In2publishCore\Domain\Model\Record::S_DELETED,
+        \In2code\In2publishCore\Domain\Model\Record::S_UNCHANGED
+    );
+}
