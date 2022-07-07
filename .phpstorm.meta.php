8ebdfff0f2c90db7c07d498271f77fd51e297ce4
Oliver Eglseder <oliver.eglseder@in2code.de>
2022-07-07 13:48:58 +0200

    [META] Add metadata for all remaining methods that expect constants

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace PHPSTORM_META {
+
     expectedReturnValues(
         \In2code\In2publishCore\Domain\Model\Record::getState(),
         \In2code\In2publishCore\Domain\Model\Record::S_ADDED,
@@ -25,4 +27,36 @@ namespace PHPSTORM_META {
         \In2code\In2publishCore\Component\TcaHandling\PreProcessing\ProcessingResult::COMPATIBLE,
         \In2code\In2publishCore\Component\TcaHandling\PreProcessing\ProcessingResult::INCOMPATIBLE
     );
+    expectedArguments(
+        \In2code\In2publishCore\Component\RemoteProcedureCall\Envelope::__construct(),
+        0,
+        \In2code\In2publishCore\Component\RemoteProcedureCall\EnvelopeDispatcher::CMD_FOLDER_EXISTS,
+        \In2code\In2publishCore\Component\RemoteProcedureCall\EnvelopeDispatcher::CMD_FILE_EXISTS,
+        \In2code\In2publishCore\Component\RemoteProcedureCall\EnvelopeDispatcher::CMD_LIST_FOLDER_CONTENTS
+    );
+    expectedArguments(
+        \In2code\In2publishCore\Config\Builder::addNode(),
+        0,
+        \In2code\In2publishCore\Config\Node\Node::T_ARRAY,
+        \In2code\In2publishCore\Config\Node\Node::T_STRICT_ARRAY,
+        \In2code\In2publishCore\Config\Node\Node::T_STRING,
+        \In2code\In2publishCore\Config\Node\Node::T_OPTIONAL_ARRAY,
+        \In2code\In2publishCore\Config\Node\Node::T_OPTIONAL_STRING,
+        \In2code\In2publishCore\Config\Node\Node::T_INTEGER,
+        \In2code\In2publishCore\Config\Node\Node::T_BOOLEAN
+    );
+    expectedArguments(
+        \In2code\In2publishCore\Testing\Data\FalStorageTestSubjectsProvider::getStorages(),
+        0,
+        \In2code\In2publishCore\Testing\Data\FalStorageTestSubjectsProvider::PURPOSE_CASE_SENSITIVITY,
+        \In2code\In2publishCore\Testing\Data\FalStorageTestSubjectsProvider::PURPOSE_DRIVER,
+        \In2code\In2publishCore\Testing\Data\FalStorageTestSubjectsProvider::PURPOSE_MISSING,
+        \In2code\In2publishCore\Testing\Data\FalStorageTestSubjectsProvider::PURPOSE_UNIQUE_TARGET
+    );
 }
+
+
+
+
+
+
