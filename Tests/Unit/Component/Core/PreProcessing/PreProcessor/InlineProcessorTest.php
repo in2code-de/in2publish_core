b8abeca81e53cbb433b97c93b1b0432794e3f985
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2022-07-18 17:59:14 +0200

    [TEST] Fix InlineProcessorTest

--- Tests/Unit/Component/Core/PreProcessing/PreProcessor/InlineProcessorTest.php
+++ Tests/Unit/Component/Core/PreProcessing/PreProcessor/InlineProcessorTest.php
@@ -4,6 +4,7 @@ namespace In2code\In2publishCore\Tests\Unit\Component\Core\PreProcessing\PreProc
 
 use In2code\In2publishCore\Component\Core\PreProcessing\PreProcessor\InlineProcessor;
 use In2code\In2publishCore\Component\Core\PreProcessing\Service\TcaEscapingMarkerService;
+use In2code\In2publishCore\Component\Core\Resolver\InlineMultiValueResolver;
 use In2code\In2publishCore\Component\Core\Resolver\InlineSelectResolver;
 use In2code\In2publishCore\Component\Core\Resolver\StaticJoinResolver;
 use In2code\In2publishCore\Tests\UnitTestCase;
@@ -29,7 +30,7 @@ class InlineProcessorTest extends UnitTestCase
         $container->method('get')->willReturn($inlineResolver);
         $inlineProcessor->injectContainer($container);
 
-        $processingResult = $inlineProcessor->process('tableNameFoo', 'fieldNameBar', $tca);
+        $processingResult = $inlineProcessor->process('table_bar', 'field_bar', $tca);
         $this->assertFalse($processingResult->isCompatible());
         $reason = $processingResult->getValue()[0];
         $this->assertSame('symmetric_field is set on the foreign side of relations, which must not be resolved',
@@ -51,7 +52,7 @@ class InlineProcessorTest extends UnitTestCase
         $container->method('get')->willReturn($inlineResolver);
         $inlineProcessor->injectContainer($container);
 
-        $processingResult = $inlineProcessor->process('tableNameFoo', 'fieldNameBar', $tca);
+        $processingResult = $inlineProcessor->process('table_bar', 'field_bar', $tca);
         $this->assertFalse($processingResult->isCompatible());
         $reason = $processingResult->getValue()[0];
         $this->assertSame('Must be set, there is no type "inline" without a foreign table', $reason);
@@ -79,10 +80,81 @@ class InlineProcessorTest extends UnitTestCase
 
         $inlineResolver->expects($this->once())->method('configure')->with('table_foo', 'foreign_field_foo',
             'foreign_table_field_foo', '');
-        $processingResult = $inlineProcessor->process('tableNameBar', 'fieldNameBar', $tca);
+        $processingResult = $inlineProcessor->process('table_bar', 'field_bar', $tca);
 
         $this->assertTrue($processingResult->isCompatible());
         $this->assertInstanceOf(InlineSelectResolver::class, $inlineResolver);
     }
-    // TODO: tests for InlineMultiValueResolver and StaticJoinResolver
+    /**
+     * @covers ::process
+     */
+    public function testInlineMultiValueResolver(): void
+    {
+        $tca1 = [
+            'type' => 'inline',
+            'foreign_table' => 'table_foo'
+        ];
+
+        $tca2 = [
+            'type' => 'inline',
+            'foreign_table' => 'table_foo',
+            'foreign_table_field' => 'foreign_table_field_foo'
+        ];
+
+        $tca3 = [
+            'type' => 'inline',
+            'foreign_table' => 'table_foo',
+            'foreign_table_field' => 'foreign_table_field_foo',
+            'foreign_match_fields' => [
+                'foreign_match_field1' => 'foreign_match_value1',
+                'foreign_match_field2' => 'foreign_match_value2'
+            ]
+        ];
+
+        $inlineProcessor = new InlineProcessor();
+        $tcaMarkerService = $this->createMock(TcaEscapingMarkerService::class);
+        $tcaMarkerService->expects($this->exactly(3))
+            ->method('escapeMarkedIdentifier')
+            ->withConsecutive(
+                [],
+                [],
+                ['foreign_match_field1 = "foreign_match_value1" AND foreign_match_field2 = "foreign_match_value2"']
+            )->willReturnOnConsecutiveCalls(
+                '',
+                '',
+                'foreign_match_field1 = "foreign_match_value1" AND foreign_match_field2 = "foreign_match_value2"'
+            );
+
+        $inlineProcessor->injectTcaEscapingMarkerService($tcaMarkerService);
+        $container = $this->createMock(Container::class);
+        $inlineResolver = $this->createMock(InlineMultiValueResolver::class);
+        $container->method('get')->willReturn($inlineResolver);
+        $inlineProcessor->injectContainer($container);
+
+        $inlineResolver->expects($this->exactly(3))
+            ->method('configure')
+            ->withConsecutive(
+                ['table_foo', 'field_bar', null, ''],
+                ['table_foo', 'field_bar', 'foreign_table_field_foo', ''],
+                [
+                    'table_foo',
+                    'field_bar',
+                    'foreign_table_field_foo',
+                    'foreign_match_field1 = "foreign_match_value1" AND foreign_match_field2 = "foreign_match_value2"'
+                ]
+            );
+
+        $processingResult1 = $inlineProcessor->process('table_bar', 'field_bar', $tca1);
+        $this->assertTrue($processingResult1->isCompatible());
+        $this->assertInstanceOf(InlineMultiValueResolver::class, $inlineResolver);
+
+        $processingResult2 = $inlineProcessor->process('table_bar', 'field_bar', $tca2);
+        $this->assertTrue($processingResult2->isCompatible());
+        $this->assertInstanceOf(InlineMultiValueResolver::class, $inlineResolver);
+
+        $processingResult2 = $inlineProcessor->process('table_bar', 'field_bar', $tca3);
+        $this->assertTrue($processingResult2->isCompatible());
+        $this->assertInstanceOf(InlineMultiValueResolver::class, $inlineResolver);
+    }
+    // TODO: test for StaticJoinResolver
 }
