fcda6000551f34e575c9f8efff16910d949c082b
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2022-07-20 10:20:20 +0200

    [TEST] Add TcaEscapingMarkerServiceTest

---
+++ Tests/Unit/Component/Core/PreProcessing/Service/TcaEscapingMarkerServiceTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace In2code\In2publishCore\Tests\Unit\Component\Core\PreProcessing\Service;
+
+use In2code\In2publishCore\Component\Core\PreProcessing\Service\TcaEscapingMarkerService;
+use In2code\In2publishCore\Tests\UnitTestCase;
+use TYPO3\CMS\Core\Database\Connection;
+
+/**
+ * @coversDefaultClass \In2code\In2publishCore\Component\Core\PreProcessing\Service\TcaEscapingMarkerService
+ */
+class TcaEscapingMarkerServiceTest extends UnitTestCase
+{
+    /**
+     * @covers ::escapeMarkedIdentifier
+     */
+    public function testEscapeMarkedIdentifierReplacesAllMarkersWithQuotedIdentifiers(): void
+    {
+        $sql = 'SELECT * FROM {#table} WHERE {#field} = {#value}';
+        $expected = 'SELECT * FROM `table` WHERE `field` = `value`';
+        $database = $this->createMock(Connection::class);
+        $database->expects($this
+            ->exactly(3))
+            ->method('quoteIdentifier')
+            ->withConsecutive(
+                ['table'],
+                ['field'],
+                ['value']
+            )->willReturnOnConsecutiveCalls(
+            '`table`',
+            '`field`',
+            '`value`'
+        );
+        $tcaEscapingMarkerService = new TcaEscapingMarkerService($database);
+        $result = $tcaEscapingMarkerService->escapeMarkedIdentifier($sql);
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * @covers ::escapeMarkedIdentifier
+     */
+    public function testEscapeMarkedIdentifierOnlyReplacesValidMarkers(): void
+    {
+        $sql = "SELECT * FROM '#table' WHERE {#field} = {#value}";
+        $expected = "SELECT * FROM '#table' WHERE 'field' = 'value'";
+        $database = $this->createMock(Connection::class);
+        $database->expects($this
+            ->exactly(2))
+            ->method('quoteIdentifier')
+            ->withConsecutive(
+                ['field'],
+                ['value']
+            )->willReturnOnConsecutiveCalls(
+                '\'field\'',
+                '\'value\''
+            );
+        $tcaEscapingMarkerService = new TcaEscapingMarkerService($database);
+        $result = $tcaEscapingMarkerService->escapeMarkedIdentifier($sql);
+        $this->assertSame($expected, $result);
+    }
+}
