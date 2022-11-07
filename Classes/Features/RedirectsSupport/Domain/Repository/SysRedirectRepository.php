bf0c90e2d6afc8dc7d2cce9497efe0ce874b4d00
Oliver Eglseder <oliver.eglseder@in2code.de>
2022-11-07 14:07:41 +0100

    [CLEANUP] Remove unused methods from SysRedirectRepository

--- Classes/Features/RedirectsSupport/Domain/Repository/SysRedirectRepository.php
+++ Classes/Features/RedirectsSupport/Domain/Repository/SysRedirectRepository.php
@@ -31,90 +31,11 @@ namespace In2code\In2publishCore\Features\RedirectsSupport\Domain\Repository;
 
 use In2code\In2publishCore\CommonInjection\LocalDatabaseInjection;
 use In2code\In2publishCore\Features\RedirectsSupport\Domain\Model\Dto\Redirect;
-use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
-use TYPO3\CMS\Core\Http\Uri;
 
 class SysRedirectRepository
 {
     use LocalDatabaseInjection;
-
-    /**
-     * @param array<Uri> $uris
-     */
-    public function findRawByUris(Connection $connection, array $uris, array $exceptUid): array
-    {
-        $query = $connection->createQueryBuilder();
-        $query->getRestrictions()->removeAll();
-
-        $predicates = [];
-
-        foreach ($uris as $uri) {
-            $target = $uri->getPath();
-            $queryPart = $uri->getQuery();
-            if (!empty($queryPart)) {
-                $target .= '?' . $queryPart;
-            }
-            if ($uri->getHost() === '*') {
-                // If the "parent" redirect does not have a host it does not matter which host the redirect has, which
-                // redirects to the host-less redirect. It just belongs.
-                $predicates[] = $query->expr()->eq('target', $query->createNamedParameter($target));
-            } else {
-                /** @psalm-suppress ImplicitToStringCast */
-                $predicates[] = $query->expr()->andX(
-                    $query->expr()->eq('target', $query->createNamedParameter($target)),
-                    $query->expr()->orX(
-                        $query->expr()->eq('source_host', $query->createNamedParameter($uri->getHost())),
-                        $query->expr()->eq('source_host', $query->createNamedParameter('*'))
-                    )
-                );
-            }
-        }
-
-        if (!empty($exceptUid)) {
-            foreach ($exceptUid as &$uid) {
-                $uid = (int)$uid;
-            }
-            unset($uid);
-            /** @psalm-suppress ImplicitToStringCast */
-            $predicates = [
-                $query->expr()->andX(
-                    $query->expr()->notIn('uid', $exceptUid),
-                    $query->expr()->orX(...$predicates)
-                ),
-            ];
-        }
-
-        $query->select('*')->from('sys_redirect')->where(...$predicates);
-        return $query->execute()->fetchAllAssociative();
-    }
-
-    public function findRawByUids(Connection $connection, array $uids): array
-    {
-        foreach ($uids as &$uid) {
-            $uid = (int)$uid;
-        }
-        unset($uid);
-        $query = $connection->createQueryBuilder();
-        $query->getRestrictions()->removeAll();
-        $query->select('*')
-              ->from('sys_redirect')
-              ->where($query->expr()->in('uid', $uids));
-        return $query->execute()->fetchAllAssociative();
-    }
-
-    public function findByRawTarget(Connection $connection, string $target, array $except): array
-    {
-        $query = $connection->createQueryBuilder();
-        $query->getRestrictions()->removeAll();
-        $query->select('*')
-              ->from('sys_redirect')
-              ->where($query->expr()->eq('target', $query->createNamedParameter($target)));
-        if (!empty($except)) {
-            $query->andWhere($query->expr()->notIn('uid', $except));
-        }
-        return $query->execute()->fetchAllAssociative();
-    }
 
     public function findHostsOfRedirects(): array
     {
