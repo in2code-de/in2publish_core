c08d37ce4277494a8f648c652f551b4ddc54b592
Oliver Eglseder <oliver.eglseder@in2code.de>
2023-10-24 12:46:37 +0200

    [REFACTOR] Import all used classes in ext_*.php files
    This is possible since at least TYPO3 v11, because the file contents are wrapped in namespaces in the cache file

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,48 @@
 <?php
 
-/** @noinspection PhpFullyQualifiedNameUsageInspection */
+use In2code\In2publishCore\Component\ConfigContainer\ConfigContainer;
+use In2code\In2publishCore\Component\ConfigContainer\Definer\In2publishCoreDefiner;
+use In2code\In2publishCore\Component\ConfigContainer\Definer\SshConnectionDefiner;
+use In2code\In2publishCore\Component\ConfigContainer\Migration\IngoredFieldsMigration;
+use In2code\In2publishCore\Component\ConfigContainer\PostProcessor\DynamicValueProvider\DynamicValueProviderRegistry;
+use In2code\In2publishCore\Component\ConfigContainer\PostProcessor\DynamicValueProvider\EnvVarProvider;
+use In2code\In2publishCore\Component\ConfigContainer\PostProcessor\DynamicValuesPostProcessor;
+use In2code\In2publishCore\Component\ConfigContainer\Provider\DefaultProvider;
+use In2code\In2publishCore\Component\ConfigContainer\Provider\FileProvider;
+use In2code\In2publishCore\Component\ConfigContainer\Provider\PageTsProvider;
+use In2code\In2publishCore\Component\ConfigContainer\Provider\UserTsProvider;
+use In2code\In2publishCore\Component\ConfigContainer\Provider\VersionedFileProvider;
+use In2code\In2publishCore\Component\Core\Record\Model\Extension\RecordExtensionTrait;
+use In2code\In2publishCore\Component\RemoteCommandExecution\RemoteAdapter\RemoteAdapterRegistry;
+use In2code\In2publishCore\Component\RemoteCommandExecution\RemoteAdapter\SshAdapter as RemoteCommandExecutionSshAdapter;
+use In2code\In2publishCore\Component\TemporaryAssetTransmission\TransmissionAdapter\SshAdapter as TransmissionSshAdapter;
+use In2code\In2publishCore\Component\TemporaryAssetTransmission\TransmissionAdapter\TransmissionAdapterRegistry;
+use In2code\In2publishCore\Controller\FrontendController;
+use In2code\In2publishCore\Features\HideRecordsDeletedDifferently\Config\Definer\HideRecordsDeletedDifferentlyDefiner;
+use In2code\In2publishCore\Features\PublishSorting\Config\Definer\PublishSortingDefiner;
+use In2code\In2publishCore\Features\RedirectsSupport\Config\Definer\RedirectsSupportDefiner;
+use In2code\In2publishCore\Features\WarningOnForeign\Config\Definer\WarningOnForeignDefiner;
+use In2code\In2publishCore\Log\Processor\BackendUserProcessor;
+use In2code\In2publishCore\Log\Processor\PublishingFailureCollector;
+use In2code\In2publishCore\Middleware\BackendRouteInitialization;
+use In2code\In2publishCore\Service\Context\ContextService;
+use In2code\In2publishCore\Testing\Tests\SshConnection\SftpRequirementsTest;
+use In2code\In2publishCore\Testing\Tests\SshConnection\SshConnectionTest;
+use In2code\In2publishCore\Testing\Tests\SshConnection\SshFunctionAvailabilityTest;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Log\LogLevel;
+use TYPO3\CMS\Core\Log\Writer\DatabaseWriter;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask;
 
-(static function () {
+(static function (): void {
     /***************************************************** Guards *****************************************************/
     if (!defined('TYPO3_REQUESTTYPE')) {
         die('Access denied.');
     }
-    if (!class_exists(\In2code\In2publishCore\Service\Context\ContextService::class)) {
+    if (!class_exists(ContextService::class)) {
         // Early return when installing per ZIP: autoload is not yet generated
         return;
     }
@@ -16,11 +51,11 @@
     // Issue: https://forge.typo3.org/issues/95962
     // Patch: https://review.typo3.org/c/Packages/TYPO3.CMS/+/72160
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Middleware\BackendRouteInitialization::class] = [
-        'className' => \In2code\In2publishCore\Middleware\BackendRouteInitialization::class,
+        'className' => BackendRouteInitialization::class,
     ];
 
     /************************************************ Record Extension ************************************************/
-    $file = \TYPO3\CMS\Core\Core\Environment::getVarPath() . '/cache/code/content_publisher/record_extension_trait.php';
+    $file = Environment::getVarPath() . '/cache/code/content_publisher/record_extension_trait.php';
     if (file_exists($file)) {
         // Initialize the variable to be able to use it in a reference
         $autoloadFn = null;
@@ -30,7 +65,7 @@
          * so it will not interfere with further class loading.
          */
         $autoloadFn = static function (string $class) use (&$autoloadFn, $file): void {
-            if (\In2code\In2publishCore\Component\Core\Record\Model\Extension\RecordExtensionTrait::class === $class) {
+            if (RecordExtensionTrait::class === $class) {
                 spl_autoload_unregister($autoloadFn);
                 unset($autoloadFn);
                 include $file;
@@ -40,21 +75,11 @@
     }
 
     /*********************************************** Settings/Instances ***********************************************/
-    $extConf = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class,
-    )->get('in2publish_core');
-    $contextService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \In2code\In2publishCore\Service\Context\ContextService::class,
-    );
-    $configContainer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \In2code\In2publishCore\Component\ConfigContainer\ConfigContainer::class,
-    );
-    $remoteAdapterRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \In2code\In2publishCore\Component\RemoteCommandExecution\RemoteAdapter\RemoteAdapterRegistry::class,
-    );
-    $transmissionAdapterRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \In2code\In2publishCore\Component\TemporaryAssetTransmission\TransmissionAdapter\TransmissionAdapterRegistry::class,
-    );
+    $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('in2publish_core');
+    $contextService = GeneralUtility::makeInstance(ContextService::class);
+    $configContainer = GeneralUtility::makeInstance(ConfigContainer::class);
+    $remoteAdapterRegistry = GeneralUtility::makeInstance(RemoteAdapterRegistry::class);
+    $transmissionAdapterRegistry = GeneralUtility::makeInstance(TransmissionAdapterRegistry::class);
 
     /************************************************** Init Caching **************************************************/
     if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['in2publish_core'])) {
@@ -63,115 +88,86 @@
 
     /************************************************** Init Logging **************************************************/
     $logLevel = $extConf['logLevel'];
-    $logLevel = \TYPO3\CMS\Core\Log\LogLevel::getInternalName((int)$logLevel);
+    $logLevel = LogLevel::getInternalName((int)$logLevel);
     $GLOBALS['TYPO3_CONF_VARS']['LOG']['In2code']['In2publishCore'] = [
         'writerConfiguration' => [
             $logLevel => [
-                \TYPO3\CMS\Core\Log\Writer\DatabaseWriter::class => [
+                DatabaseWriter::class => [
                     'logTable' => 'tx_in2publishcore_log',
                 ],
             ],
         ],
         'processorConfiguration' => [
             $logLevel => [
-                \In2code\In2publishCore\Log\Processor\BackendUserProcessor::class => [],
-                \In2code\In2publishCore\Log\Processor\PublishingFailureCollector::class => [],
+                BackendUserProcessor::class => [],
+                PublishingFailureCollector::class => [],
             ],
         ],
     ];
 
     /**************************************** Register Config Definer/Provider ****************************************/
-    $configContainer->registerDefiner(
-        \In2code\In2publishCore\Component\ConfigContainer\Definer\In2publishCoreDefiner::class,
-    );
-    $configContainer->registerDefiner(
-        \In2code\In2publishCore\Features\WarningOnForeign\Config\Definer\WarningOnForeignDefiner::class,
-    );
-    $configContainer->registerDefiner(
-        \In2code\In2publishCore\Features\PublishSorting\Config\Definer\PublishSortingDefiner::class,
-    );
+    $configContainer->registerDefiner(In2publishCoreDefiner::class);
+    $configContainer->registerDefiner(WarningOnForeignDefiner::class);
+    $configContainer->registerDefiner(PublishSortingDefiner::class);
     if (
         $contextService->isForeign()
         || 'ssh' === $remoteAdapterRegistry->getSelectedAdapter()
         || 'ssh' === $transmissionAdapterRegistry->getSelectedAdapter()
     ) {
-        $configContainer->registerDefiner(
-            \In2code\In2publishCore\Component\ConfigContainer\Definer\SshConnectionDefiner::class,
-        );
+        $configContainer->registerDefiner(SshConnectionDefiner::class);
     }
-    $configContainer->registerDefiner(
-        \In2code\In2publishCore\Features\HideRecordsDeletedDifferently\Config\Definer\HideRecordsDeletedDifferentlyDefiner::class,
-    );
+    $configContainer->registerDefiner(HideRecordsDeletedDifferentlyDefiner::class);
 
-    $configContainer->registerProvider(
-        \In2code\In2publishCore\Component\ConfigContainer\Provider\DefaultProvider::class,
-    );
-    $configContainer->registerProvider(\In2code\In2publishCore\Component\ConfigContainer\Provider\FileProvider::class);
-    $configContainer->registerProvider(
-        \In2code\In2publishCore\Component\ConfigContainer\Provider\PageTsProvider::class,
-    );
-    $configContainer->registerProvider(
-        \In2code\In2publishCore\Component\ConfigContainer\Provider\VersionedFileProvider::class,
-    );
+    $configContainer->registerProvider(DefaultProvider::class);
+    $configContainer->registerProvider(FileProvider::class);
+    $configContainer->registerProvider(PageTsProvider::class);
+    $configContainer->registerProvider(VersionedFileProvider::class);
     if (!$extConf['disableUserConfig']) {
-        $configContainer->registerProvider(
-            \In2code\In2publishCore\Component\ConfigContainer\Provider\UserTsProvider::class,
-        );
+        $configContainer->registerProvider(UserTsProvider::class);
     }
-    $configContainer->registerPostProcessor(
-        \In2code\In2publishCore\Component\ConfigContainer\PostProcessor\DynamicValuesPostProcessor::class,
-    );
+    $configContainer->registerPostProcessor(DynamicValuesPostProcessor::class);
 
-    $dynamicValueProviderRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \In2code\In2publishCore\Component\ConfigContainer\PostProcessor\DynamicValueProvider\DynamicValueProviderRegistry::class,
-    );
-    $dynamicValueProviderRegistry->registerDynamicValue(
-        'env',
-        \In2code\In2publishCore\Component\ConfigContainer\PostProcessor\DynamicValueProvider\EnvVarProvider::class,
-    );
+    $dynamicValueProviderRegistry = GeneralUtility::makeInstance(DynamicValueProviderRegistry::class);
+    $dynamicValueProviderRegistry->registerDynamicValue('env', EnvVarProvider::class);
 
-    $configContainer->registerMigration(
-        \In2code\In2publishCore\Component\ConfigContainer\Migration\IngoredFieldsMigration::class,
-    );
+    $configContainer->registerMigration(IngoredFieldsMigration::class);
 
     /******************************************** Configure Compare Plugin ********************************************/
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    ExtensionUtility::configurePlugin(
         'in2publish_core',
         'Pi1',
-        [\In2code\In2publishCore\Controller\FrontendController::class => 'preview'],
-        [\In2code\In2publishCore\Controller\FrontendController::class => 'preview'],
+        [FrontendController::class => 'preview'],
+        [FrontendController::class => 'preview'],
     );
 
     /******************************************** Configure Garbage Collector  ****************************************/
     // Register table tx_in2publishcore_running_request  in table garbage collector
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask::class]['options']['tables']['tx_in2publishcore_running_request'] = [
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][TableGarbageCollectionTask::class]['options']['tables']['tx_in2publishcore_running_request'] = [
         'dateField' => 'timestamp_begin',
         'expirePeriod' => 1,
     ];
 
     /***************************************** Register Communication Adapter *****************************************/
     $remoteAdapterRegistry->registerAdapter(
-        \In2code\In2publishCore\Component\RemoteCommandExecution\RemoteAdapter\SshAdapter::ADAPTER_KEY,
-        \In2code\In2publishCore\Component\RemoteCommandExecution\RemoteAdapter\SshAdapter::class,
+        RemoteCommandExecutionSshAdapter::ADAPTER_KEY,
+        RemoteCommandExecutionSshAdapter::class,
         'LLL:EXT:in2publish_core/Resources/Private/Language/locallang.xlf:adapter.remote.ssh',
         [
-            \In2code\In2publishCore\Testing\Tests\SshConnection\SshFunctionAvailabilityTest::class,
-            \In2code\In2publishCore\Testing\Tests\SshConnection\SshConnectionTest::class,
+            SshFunctionAvailabilityTest::class,
+            SshConnectionTest::class,
         ],
     );
     $transmissionAdapterRegistry->registerAdapter(
-        \In2code\In2publishCore\Component\TemporaryAssetTransmission\TransmissionAdapter\SshAdapter::ADAPTER_KEY,
-        \In2code\In2publishCore\Component\TemporaryAssetTransmission\TransmissionAdapter\SshAdapter::class,
+        TransmissionSshAdapter::ADAPTER_KEY,
+        TransmissionSshAdapter::class,
         'LLL:EXT:in2publish_core/Resources/Private/Language/locallang.xlf:adapter.transmission.ssh',
         [
-            \In2code\In2publishCore\Testing\Tests\SshConnection\SshFunctionAvailabilityTest::class,
-            \In2code\In2publishCore\Testing\Tests\SshConnection\SshConnectionTest::class,
-            \In2code\In2publishCore\Testing\Tests\SshConnection\SftpRequirementsTest::class,
+            SshFunctionAvailabilityTest::class,
+            SshConnectionTest::class,
+            SftpRequirementsTest::class,
         ],
     );
 
     /************************************************ Redirect Support ************************************************/
-    $configContainer->registerDefiner(
-        \In2code\In2publishCore\Features\RedirectsSupport\Config\Definer\RedirectsSupportDefiner::class,
-    );
+    $configContainer->registerDefiner(RedirectsSupportDefiner::class);
 })();
