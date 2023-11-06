c97f20adfce33537aa1ed2cb9ac0c5dd0bc5236d
Daniel Hoffmann <daniel.hoffmann@in2code.de>
2023-11-06 13:00:41 +0100

    [REFACTOR] Changes backend module registration for m1, m3, m5
    The backend modules overview, file & redirects are now registered via Configuration/Backend/Modules.php
    For TYPO3 v11 they are registered via the old way (ext_tables.php)
    
    The Icons are provided via Icons.php and can be retrieved from the IconFactory now.
    
    Resolves: https://projekte.in2code.de/issues/60077

---
+++ Configuration/Icons.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright notice
+ *
+ * (c) 2023 in2code.de and the following authors:
+ * Daniel Hoffmann <daniel.hoffmann@in2code.de>
+ *
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'in2publish-core-overview-module' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:in2publish_core/Resources/Public/Icons/Overview.svg',
+    ],
+    'in2publish-core-file-module' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:in2publish_core/Resources/Public/Icons/File.svg',
+    ],
+    'in2publish-core-redirect-module' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:in2publish_core/Resources/Public/Icons/Redirect.svg',
+    ],
+];
