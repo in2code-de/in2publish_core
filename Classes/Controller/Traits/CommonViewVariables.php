1d951acff144e69886bb330ca20ed913cbb88976
Oliver Eglseder <oliver.eglseder@in2code.de>
2023-11-07 12:52:04 +0100

    [BUGFIX] Use the objects view property instead of initializeView arguments

--- Classes/Controller/Traits/CommonViewVariables.php
+++ Classes/Controller/Traits/CommonViewVariables.php
@@ -7,7 +7,6 @@ namespace In2code\In2publishCore\Controller\Traits;
 use In2code\In2publishCore\Component\ConfigContainer\ConfigContainerInjection;
 use In2code\In2publishCore\Service\Environment\EnvironmentServiceInjection;
 use In2code\In2publishCore\Service\Extension\ExtensionServiceInjection;
-use TYPO3\CMS\Extbase\Mvc\View\ViewInterface;
 
 trait CommonViewVariables
 {
@@ -15,10 +14,10 @@ trait CommonViewVariables
     use EnvironmentServiceInjection;
     use ExtensionServiceInjection;
 
-    protected function initializeView(ViewInterface $view): void
+    protected function initializeView(): void
     {
-        $view->assign('extensionVersion', $this->extensionService->getExtensionVersion('in2publish_core'));
-        $view->assign('config', $this->configContainer->get());
-        $view->assign('testStatus', $this->environmentService->getTestStatus());
+        $this->view->assign('extensionVersion', $this->extensionService->getExtensionVersion('in2publish_core'));
+        $this->view->assign('config', $this->configContainer->get());
+        $this->view->assign('testStatus', $this->environmentService->getTestStatus());
     }
 }
