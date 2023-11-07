534d9b8df71398ac9f7e4d1f8ab968c071645e7b
Oliver Eglseder <oliver.eglseder@in2code.de>
2023-11-07 14:01:34 +0100

    [BUGFIX] Overwrite callActionMethod instead of initializeView to prevent version issues

--- Classes/Controller/Traits/CommonViewVariables.php
+++ Classes/Controller/Traits/CommonViewVariables.php
@@ -7,6 +7,8 @@ namespace In2code\In2publishCore\Controller\Traits;
 use In2code\In2publishCore\Component\ConfigContainer\ConfigContainerInjection;
 use In2code\In2publishCore\Service\Environment\EnvironmentServiceInjection;
 use In2code\In2publishCore\Service\Extension\ExtensionServiceInjection;
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Extbase\Mvc\RequestInterface;
 
 trait CommonViewVariables
 {
@@ -14,10 +16,11 @@ trait CommonViewVariables
     use EnvironmentServiceInjection;
     use ExtensionServiceInjection;
 
-    protected function initializeView(): void
+    protected function callActionMethod(RequestInterface $request): ResponseInterface
     {
         $this->view->assign('extensionVersion', $this->extensionService->getExtensionVersion('in2publish_core'));
         $this->view->assign('config', $this->configContainer->get());
         $this->view->assign('testStatus', $this->environmentService->getTestStatus());
+        return parent::callActionMethod($request);
     }
 }
