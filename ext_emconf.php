d32659ab29209b5327546b6ab05e4a6761d7e5f8
Oliver Eglseder <oliver.eglseder@in2code.de>
2023-11-30 15:08:37 +0100

    [META] Mark TYPO3 v12 as compatible in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.4.0-8.99.99',
-            'typo3' => '11.5.0-11.99.99',
+            'typo3' => '11.5.0-12.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
