4b2d97b37c43d3caa8dfb8e086d8bbbaf15f7bb3
Oliver Eglseder <oliver.eglseder@in2code.de>
2023-12-05 12:43:58 +0100

    [BUGFIX] Resolve conditions and listeners once

--- Classes/Features/ConditionalEventListener/EventListener/ConditionalEventListenerDelegator.php
+++ Classes/Features/ConditionalEventListener/EventListener/ConditionalEventListenerDelegator.php
@@ -7,11 +7,14 @@ namespace In2code\In2publishCore\Features\ConditionalEventListener\EventListener
 use In2code\In2publishCore\Component\ConfigContainer\ConfigContainer;
 use Psr\Container\ContainerInterface;
 
+use function get_class;
+
 class ConditionalEventListenerDelegator
 {
     protected ContainerInterface $container;
     protected ConfigContainer $configContainer;
     protected array $eventListeners;
+    protected array $evaluatedListeners = [];
 
     public function __construct(ContainerInterface $container, ConfigContainer $configContainer, array $eventListeners)
     {
@@ -22,17 +25,27 @@ class ConditionalEventListenerDelegator
 
     public function __invoke(object $event): void
     {
+        $listeners = $this->getListenersForEvent($event);
+        foreach ($listeners as $listener) {
+            $listener['service']->{$listener['method']}($event);
+        }
+    }
+
+    protected function getListenersForEvent(object $event): array
+    {
         $eventName = get_class($event);
-        foreach ($this->eventListeners[$eventName] ?? [] as $identifier => $listener) {
-            if ($this->configContainer->get($listener['condition'])) {
-                $service = $this->container->get($listener['service']);
-                $service->{$listener['method']}($event);
-            } else {
-                unset($this->eventListeners[$eventName][$identifier]);
-                if (empty($this->eventListeners[$eventName])) {
-                    unset($this->eventListeners[$eventName]);
+        if (!isset($this->evaluatedListeners[$eventName])) {
+            $this->evaluatedListeners[$eventName] = [];
+            $eventListeners = $this->eventListeners[$eventName] ?? [];
+            foreach ($eventListeners as $identifier => $eventListener) {
+                if ($this->configContainer->get($eventListener['condition'])) {
+                    $service = $this->container->get($eventListener['service']);
+
+                    $this->evaluatedListeners[$eventName][$identifier] = $eventListener;
+                    $this->evaluatedListeners[$eventName][$identifier]['service'] = $service;
                 }
             }
         }
+        return $this->evaluatedListeners[$eventName];
     }
 }
