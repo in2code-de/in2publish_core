2794a9aeb1cf156c832919ae1a617d2788dff8cb
Oliver Eglseder <oliver.eglseder@in2code.de>
2024-01-19 11:03:50 +0100

    [TEST] Fix ReplaceAndRenameFileInstructionTest

--- Tests/Unit/Component/Core/Publisher/Instruction/ReplaceAndRenameFileInstructionTest.php
+++ Tests/Unit/Component/Core/Publisher/Instruction/ReplaceAndRenameFileInstructionTest.php
@@ -17,19 +17,20 @@ class ReplaceAndRenameFileInstructionTest extends UnitTestCase
     /**
      * @covers ::execute
      */
-    public function testExecutionCreatesFolder(): void
+    public function testExecutionRenamesAndReplacesFile(): void
     {
         $driver = $this->createMock(LocalDriver::class);
-        $driver->expects($this->once())->method('folderExists')->with('someOtherFolder')->willReturn(true);
-        $driver->expects($this->once())->method('moveFileWithinStorage')->with(
-            'new_folder/oldFile.txt',
-            'someOtherFolder',
-            'newFileName.txt',
-        );
+        $driver->expects($this->once())->method('renameFile')->with('new_folder/oldFile.txt', 'newFileName.txt');
+        $driver->expects($this->once())->method('replaceFile')->with('new_folder/newFileName.txt', '/tmp/foo.txt');
 
         $falDriverService = $this->createMock(FalDriverService::class);
         $falDriverService->method('getDriver')->willReturn($driver);
-        $instruction = new ReplaceAndRenameFileInstruction(1, 'new_folder/oldFile.txt', 'someOtherFolder/newFileName.txt', '/tmp/foo.txt');
+        $instruction = new ReplaceAndRenameFileInstruction(
+            1,
+            'new_folder/oldFile.txt',
+            'new_folder/newFileName.txt',
+            '/tmp/foo.txt',
+        );
         $instruction->execute($falDriverService);
     }
 }
