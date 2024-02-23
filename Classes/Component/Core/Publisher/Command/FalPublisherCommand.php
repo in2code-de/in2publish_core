b24da6a7ca3d60b367e0805634c13ed60ac2d2b9
Oliver Eglseder <oliver.eglseder@in2code.de>
2024-02-23 13:08:30 +0100

    [BUGFIX] Replace array unpacking by string keys with reflection for PHP 7.4 support
    Resolves: #109
    Resolves: #110
    Resolves: https://projekte.in2code.de/issues/62310

--- Classes/Component/Core/Publisher/Command/FalPublisherCommand.php
+++ Classes/Component/Core/Publisher/Command/FalPublisherCommand.php
@@ -8,6 +8,7 @@ use In2code\In2publishCore\CommonInjection\LocalDatabaseInjection;
 use In2code\In2publishCore\Component\Core\DemandResolver\Filesystem\Service\FalDriverServiceInjection;
 use In2code\In2publishCore\Component\Core\Publisher\Instruction\PublishInstruction;
 use In2code\In2publishCore\Service\Context\ContextServiceInjection;
+use ReflectionClass;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -16,6 +17,9 @@ use TYPO3\CMS\Core\Database\Connection;
 
 use function explode;
 use function json_decode;
+use function ksort;
+
+use const JSON_THROW_ON_ERROR;
 
 class FalPublisherCommand extends Command
 {
@@ -54,12 +58,28 @@ class FalPublisherCommand extends Command
         $instructions = [];
         foreach ($rows as $row) {
             $arguments = json_decode($row['configuration'], true, 512, JSON_THROW_ON_ERROR);
-            $instructions[] = new $row['instruction'](...$arguments);
+            $instructionClass = $row['instruction'];
+            $constructorArguments = $this->mapArgumentsToConstructorParameters($instructionClass, $arguments);
+            $instructions[] = new $instructionClass(...$constructorArguments);
         }
 
         foreach ($instructions as $instruction) {
             $instruction->execute($this->falDriverService);
         }
         return Command::SUCCESS;
+    }
+
+    protected function mapArgumentsToConstructorParameters(string $instructionClass, array $arguments): array
+    {
+        $reflection = new ReflectionClass($instructionClass);
+        $constructorArguments = [];
+        $constructorReflection = $reflection->getConstructor();
+        if ($constructorReflection) {
+            foreach ($constructorReflection->getParameters() as $parameter) {
+                $constructorArguments[$parameter->getPosition()] = $arguments[$parameter->getName()];
+            }
+            ksort($constructorArguments);
+        }
+        return $constructorArguments;
     }
 }
