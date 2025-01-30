913114916d5033a71996e4916605ec47ed1a6152
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2025-01-30 16:58:13 +0100

    [TEST] Fix PublishChangedContentTest

--- Tests/Browser/PublishChangedContentTest.php
+++ Tests/Browser/PublishChangedContentTest.php
@@ -17,7 +17,7 @@ class PublishChangedContentTest extends AbstractBrowserTestCase
     public function testChangedPageContentCanBePublished(): void
     {
         $localDriver = WebDriverFactory::createChromeDriver();
-        TYPO3Helper::backendLogin($localDriver, 'https://local.v12.in2publish-core.de/typo3', 'admin', 'password');
+        TYPO3Helper::backendLogin($localDriver, 'https://local.v13.in2publish-core.de/typo3', 'admin', 'password');
 
         TYPO3Helper::selectModuleByText($localDriver, 'Page');
         TYPO3Helper::selectInPageTree(
@@ -55,7 +55,7 @@ class PublishChangedContentTest extends AbstractBrowserTestCase
         unset($localDriver);
 
         $foreignDriver = WebDriverFactory::createChromeDriver();
-        TYPO3Helper::backendLogin($foreignDriver, 'https://foreign.v12.in2publish-core.de/typo3', 'admin', 'password');
+        TYPO3Helper::backendLogin($foreignDriver, 'https://foreign.v13.in2publish-core.de/typo3', 'admin', 'password');
         TYPO3Helper::selectModuleByText($foreignDriver, 'Page');
         TYPO3Helper::selectInPageTree(
             $foreignDriver,
@@ -66,7 +66,13 @@ class PublishChangedContentTest extends AbstractBrowserTestCase
         sleep($this->sleepTime);
 
         TYPO3Helper::inContentIFrameContext($foreignDriver, static function (WebDriver $driver): void {
-            $driver->findElement(WebDriverBy::xpath('//div[@data-table="tt_content" and @data-uid="49"]//a[@title="Edit"]'))->click();
+            $driver->findElement(
+                WebDriverBy::xpath(
+                    '//div[contains(@class, "t3-page-ce") and @data-table="tt_content" and @data-uid="49"]' .
+                    '//div[contains(@class, "btn-group")]//a[@title="Edit"]'
+                )
+            )->click();
+
             self::assertElementContains(
                 $driver,
                 '1b.1 Header - changed',
