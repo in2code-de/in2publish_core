fe44e7efff9e7624ea5375a4b9588986e07b2a1c
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2025-02-03 17:21:24 +0100

    [TEST] Fix BackendUserPreferencesResetTest

--- Tests/Browser/Regression/BackendUserPreferencesResetTest.php
+++ Tests/Browser/Regression/BackendUserPreferencesResetTest.php
@@ -8,6 +8,7 @@ use CoStack\StackTest\TYPO3\TYPO3Helper;
 use CoStack\StackTest\WebDriver\WebDriverFactory;
 use CoStack\StackTest\WebDriver\Remote\WebDriver;
 use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
 use In2code\In2publishCore\Tests\Browser\AbstractBrowserTestCase;
 
 class BackendUserPreferencesResetTest extends AbstractBrowserTestCase
@@ -21,8 +22,15 @@ class BackendUserPreferencesResetTest extends AbstractBrowserTestCase
         $localDriver->click(WebDriverBy::linkText('User Settings'));
 
         TYPO3Helper::inContentIFrameContext($localDriver, static function (WebDriver $driver): void {
-            $driver->click(WebDriverBy::linkText('Reset configuration'));
-            $driver->click(WebDriverBy::cssSelector('[data-event-payload="resetConfiguration"]'));
+            $driver->click(WebDriverBy::xpath('//button[contains(text(), "Reset configuration")]'));
+
+
+            $driver->wait()->until(
+                WebDriverExpectedCondition::visibilityOfElementLocated(
+                    WebDriverBy::cssSelector('input[data-event-payload="resetConfiguration"]')
+                )
+            );
+            $driver->click(WebDriverBy::cssSelector('input[data-event-payload="resetConfiguration"]'));
         });
 
         TYPO3Helper::clickModalButton($localDriver, 'OK');
