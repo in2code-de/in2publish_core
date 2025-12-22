de1078a521ffba54237d28e9873b3bb01c0cce9c
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2025-12-22 16:24:39 +0100

    [TESTS][BUGFIX] Fix base urls for playwright tests

--- Tests/Playwright/config.ts
+++ Tests/Playwright/config.ts
@@ -1,10 +1,10 @@
 export default {
   // For the sake of using relative urls in page.goto('module/...') the trailing slash is needed
   local: {
-    baseUrl: process.env.PLAYWRIGHT_BASE_URL || 'https://local.v13.in2publish.de/typo3/',
+    baseUrl: process.env.PLAYWRIGHT_BASE_URL || 'https://local.v13.in2publish-core.de/typo3/',
   },
   foreign: {
-    baseUrl: process.env.PLAYWRIGHT_FOREIGN_BASE_URL || 'https://foreign.v13.in2publish.de/typo3/',
+    baseUrl: process.env.PLAYWRIGHT_FOREIGN_BASE_URL || 'https://foreign.v13.in2publish-core.de/typo3/',
   },
   login: {
     admin: {
